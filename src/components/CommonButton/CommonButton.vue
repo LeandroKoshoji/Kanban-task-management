6ecfe0f7c037a5a23b5fb0fc26ac2ce5d765d45a
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-06-30 17:20:00 -0300

    feat(components/CommonButton): create default button

---
+++ src/components/CommonButton/CommonButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    class="heading-m"
+    :class="{
+      [heightSize]: true,
+      [widthSize]: true,
+      ['btn-color-' + color]: true
+    }"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+interface Props {
+  heightSize: string
+  widthSize: string
+  color: string
+}
+const props = defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+@import './CommonButton.scss';
+</style>
