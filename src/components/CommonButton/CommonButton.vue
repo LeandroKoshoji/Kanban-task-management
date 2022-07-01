d0914ed5bb319d72aaec7585dd0941d5c9ca66c7
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-06-30 23:19:25 -0300

    refactor(components/CommonButton): add class to computed property

--- src/components/CommonButton/CommonButton.vue
+++ src/components/CommonButton/CommonButton.vue
@@ -1,24 +1,23 @@
 <template>
-  <button
-    class="heading-m"
-    :class="{
-      [heightSize]: true,
-      [widthSize]: true,
-      ['btn-color-' + color]: true
-    }"
-  >
+  <button class="heading-m" :class="cssClass">
     <slot></slot>
   </button>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue'
+import { defineProps, computed } from 'vue'
 interface Props {
   heightSize: string
   widthSize: string
   color: string
 }
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const cssClass = computed(() => ({
+  ['height-' + props.heightSize]: true,
+  ['width-' + props.widthSize]: true,
+  ['btn-color-' + props.color]: true
+}))
 </script>
 
 <style scoped lang="scss">
