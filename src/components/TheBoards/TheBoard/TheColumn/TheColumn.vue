09934458b6904456d637a6312d2ba6b0957b36aa
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-07-08 00:09:25 -0300

    feat(components/TheBoard): add the column and the menu components

---
+++ src/components/TheBoards/TheBoard/TheColumn/TheColumn.vue
@@ -0,0 +1,92 @@
+<template>
+  <Draggable class="column-wrapper">
+    <div class="column-tasks" v-if="props.tasksMode">
+      <div class="title-container">
+        <div class="title-circle">
+          <input
+            type="color"
+            class="title-circle-input"
+            v-model="circleColor"
+          />
+        </div>
+        <p class="column-title heading-s">
+          {{ props.columnName?.toUpperCase() }} ({{ props.tasks?.length }})
+        </p>
+      </div>
+      <Container class="tasks-container">
+        <TheTaskCard
+          v-for="(task, index) in props.tasks"
+          :key="index"
+          :task="task"
+        />
+      </Container>
+    </div>
+  </Draggable>
+</template>
+
+<script setup lang="ts">
+import TheTaskCard from '../TheTaskCard/TheTaskCard.vue'
+import { Draggable, Container } from 'vue3-smooth-dnd'
+import { defineProps, onBeforeMount, ref } from 'vue'
+import { generateRandomColor } from '@/utils'
+
+interface Props {
+  columnName?: string
+  tasks?: object[]
+  tasksMode?: boolean
+}
+
+const props = defineProps<Props>()
+const circleColor = ref()
+
+onBeforeMount(() => {
+  circleColor.value = generateRandomColor()
+})
+</script>
+
+<style scoped lang="scss">
+.column-wrapper {
+  height: 100%;
+  width: 280px;
+  min-width: 280px;
+  padding: 0 5px;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 1px;
+  }
+
+  .column-tasks {
+    .title-container {
+      display: flex;
+      align-items: center;
+      margin-bottom: 24px;
+      .title-circle {
+        position: relative;
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: v-bind(circleColor);
+
+        &-input {
+          position: absolute;
+          width: inherit;
+          height: inherit;
+          opacity: 0;
+          cursor: pointer;
+        }
+      }
+      .column-title {
+        padding-left: 15px;
+        color: var(--color-medium-grey);
+      }
+    }
+
+    .tasks-container {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+    }
+  }
+}
+</style>
---
+++ src/components/TheBoards/TheBoard/TheTaskCard/TheTaskCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <Draggable class="task-card" :class="themeStore.theme">
+    <p class="task-title heading-l">{{ props.task?.title }}</p>
+    <p class="subtasks body-m">0 of 6 subtasks</p>
+  </Draggable>
+</template>
+
+<script setup lang="ts">
+import { Draggable } from 'vue3-smooth-dnd'
+import { defineProps } from 'vue'
+import { useThemeStore } from '@/stores/theme'
+
+interface Props {
+  task: object
+}
+const props = defineProps<Props>()
+
+const themeStore = useThemeStore()
+</script>
+
+<style scoped lang="scss">
+.task-card {
+  width: 100%;
+  min-height: 88px;
+  border-radius: var(--border-radius);
+  background-color: var(--color-white);
+  padding: 23px 16px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px;
+
+  &.dark {
+    background-color: var(--color-dark-grey);
+    color: var(--color-white);
+  }
+
+  .subtasks {
+    color: var(--color-medium-grey);
+  }
+}
+</style>
